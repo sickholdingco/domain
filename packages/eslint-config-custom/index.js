9f851cc85b97fa6556b6ff5cdee15774fe11ebc7
Ian Kusner <40447490+ikusner13@users.noreply.github.com>
2022-11-07 19:19:16 -0500

    Lint presets (#12)
    * add to default eslint
    
    * restrict yarn version
    
    * install

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,6 +2,17 @@ module.exports = {
   extends: ["next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "@next/next/no-img-element": "off",
+    "react/jsx-curly-brace-presence": ["error", { props: "never", children: "never" }],
+    "react/self-closing-comp": ["error", { component: true, html: true }],
     "react/jsx-key": "off",
   },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: ["plugin:@typescript-eslint/recommended"],
+      plugins: ["@typescript-eslint"],
+      parser: "@typescript-eslint/parser",
+    },
+  ],
 };
